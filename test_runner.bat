10f90f0df84702922e10b4e2b1bacd2bf4b4ad74
Adam Zulqarnain <adam.zul@gmail.com>
2021-05-09 13:35:41 +0800

    Resolved dependencies for testng.xml execution, included all required .jar and .class into project

--- test_runner.bat
+++ test_runner.bat
@@ -2,8 +2,8 @@ set projectLocation=C:\Users\senheng\Documents\repo\syok-java-appium
  
 cd %projectLocation%
  
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*;%projectLocation%\target\test-classes\*
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*;%projectLocation%\bin\*
  
-java org.testng.TestNG %projectLocation%\testng.xml
+java -cp %classpath% org.testng.TestNG %projectLocation%\testng.xml -d C:\Users\senheng\Downloads
  
 pause
